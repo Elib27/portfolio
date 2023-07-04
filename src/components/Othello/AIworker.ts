fb10716ef15419dcb686aa8d0344d1377d006c7c
Eliot Bas <eliot.bas@orange.fr>
2023-07-04 15:16:02 +0200

    convert AIworker to ts

--- src/components/Othello/AIworker.ts
+++ src/components/Othello/AIworker.ts
@@ -1,19 +1,21 @@
+import type { Gameboard, Move } from './othello';
+
 const GAMEBOARD_SIZE = 8;
 
-function copyArray(src, dest) {
+function copyArray(src: Int8Array, dest: Int8Array): void {
   for (let i = 0; i < src.length; i++) {
     dest[i] = src[i];
   }
 }
 
-function convertAImoveToMove(AImove) {
+function convertAImoveToMove(AImove: number): Move{
   const row = Math.floor(AImove / GAMEBOARD_SIZE);
   const column = AImove % GAMEBOARD_SIZE;
   const move = { row, column };
   return move;
 }
 
-function convertGameboardToArray(gameboard) {
+function convertGameboardToArray(gameboard: Gameboard) {
   const gameboardArray = new Int8Array(GAMEBOARD_SIZE * GAMEBOARD_SIZE);
   for (let i = 0; i < GAMEBOARD_SIZE; i++) {
     for (let j = 0; j < GAMEBOARD_SIZE; j++) {
@@ -25,11 +27,11 @@ function convertGameboardToArray(gameboard) {
 
 WebAssembly.instantiateStreaming(fetch(new URL('./othelloAI.wasm', import.meta.url).href), {}).then(obj => {
 
-  const getAImove = obj.instance.exports.getAImove;
+  const getAImove = obj.instance.exports.getAImove as CallableFunction;
 
-  const generateAImove = async (gameboard, player, difficulty) => {
+  async function generateAImove (gameboard: Gameboard, player: string, difficulty: number): Promise<Move> {
     const arrayGameboard = convertGameboardToArray(gameboard);
-    const WASMmemory = obj.instance.exports.memory;
+    const WASMmemory = obj.instance.exports.memory as WebAssembly.Memory;
     const arrayGameboardMemory = new Int8Array(WASMmemory.buffer);
     copyArray(arrayGameboard, arrayGameboardMemory);
     const result = getAImove(arrayGameboardMemory, player.charCodeAt(0), difficulty);
@@ -37,7 +39,7 @@ WebAssembly.instantiateStreaming(fetch(new URL('./othelloAI.wasm', import.meta.u
     return move;
   }
 
-  self.onmessage = async (event) => {
+  self.onmessage = async (event: MessageEvent<{gameboard: Gameboard, player: string, difficulty: number}>) => {
     const { gameboard, player, difficulty } = event.data;
     const AImove = await generateAImove(gameboard, player, difficulty)
     self.postMessage({ move: AImove });
